b3f8e2949a9de43b74508d95029b22c542337384
Arba Marco <arba.marco@iisgalvanimi.edu.it>
2024-02-03 09:45:28 +0000

    modifiche 2

--- routing-app/src/app/app-routing.module.ts
+++ routing-app/src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { FruitsComponent } from './fruits/fruits.component';
 import { AnimalsComponent } from './animals/animals.component';
 
-const routes: Routes = [  { path: 'animals', component: AnimalsComponent},
-                          { path: 'fruits', component: FruitsComponent},
+const routes: Routes = [  
+  { path: 'animals', component: AnimalsComponent},
+  { path: 'fruits', component: FruitsComponent},
 ];
 
 @NgModule({
